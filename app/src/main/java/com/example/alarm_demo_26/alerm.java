9b28d714637a9282f8f7a0a8696f2bdf7624d311
1a813014 <72906041+1a813014@users.noreply.github.com>
2020-12-26 20:34:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/alarm_demo_26/alerm.java
@@ -0,0 +1,15 @@
+package com.example.alarm_demo_26;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+public class alerm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "鬧鐘 !", Toast.LENGTH_SHORT).show();
+        Vibrator vibrator = (Vibrator)context.getSystemService(context.VIBRATOR_SERVICE);
+        vibrator.vibrate(10000);
+    }
+}
